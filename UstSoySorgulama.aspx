917d475917d09449362ee35d99883002d2855bdd
merve <81551521+merveaydinnn@users.noreply.github.com>
2021-04-05 09:22:46 +0300

    Add files via upload

---
+++ UstSoySorgulama.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UstSoySorgulama.aspx.cs" Inherits="UstSoySorgula.UstSoySorgulama" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+             
+            
+           
+           
+           
+            
+           <h4> ÜST SOY SORGULA </h4>
+    
+              <br />
+              <br />
+    
+            
+    
+              <asp:Table ID="Table1" runat="server" Height="185px" Width="574px">
+                  <asp:TableRow runat="server">
+                      <asp:TableCell runat="server">
+                          <asp:Label ID="Label1" runat="server" Text="Tc Kimlik Numarası : " > </asp:Label>
+                          </asp:TableCell>
+                      <asp:TableCell runat="server"> 
+                          <asp:TextBox ID="txtTcKimlikNo" runat="server" MaxLength="11"></asp:TextBox>
+                              </asp:TableCell>
+                  </asp:TableRow>
+                  <asp:TableRow runat="server">
+                      <asp:TableCell runat="server">   <asp:Label ID="Label2" runat="server" Text="Adınız : "> </asp:Label> </asp:TableCell>
+                      <asp:TableCell runat="server"> <asp:TextBox ID="txtAd" runat="server"></asp:TextBox></asp:TableCell>
+                  </asp:TableRow>
+                  <asp:TableRow runat="server">
+                       <asp:TableCell runat="server">   <asp:Label ID="Label3" runat="server" Text="Soyadını : "> </asp:Label> </asp:TableCell>
+                      <asp:TableCell runat="server"> <asp:TextBox ID="txtSoyad" runat="server"></asp:TextBox></asp:TableCell>
+                   
+                  </asp:TableRow>
+                  <asp:TableRow runat="server">
+                      <asp:TableCell runat="server"> <asp:Label ID="Label4" runat="server" Text="Dogum Yiliniz : "> </asp:Label> </asp:TableCell>
+                      <asp:TableCell runat="server"> <asp:TextBox ID="txtDogumYili" runat="server" MaxLength="4"></asp:TextBox></asp:TableCell>
+                  </asp:TableRow>
+                  <asp:TableRow runat="server">
+                      <asp:TableCell runat="server"></asp:TableCell>
+                      <asp:TableCell runat="server"><asp:Button ID="BtnSorgula" runat="server" Text="Sorgula" OnClick="Sorgula_Click" Width="126px" /></asp:TableCell>
+                  </asp:TableRow>
+                  <asp:TableRow runat="server">
+                      <asp:TableCell runat="server"> <asp:Label ID="Label5" runat="server" > </asp:Label> </asp:TableCell>
+                  </asp:TableRow>
+                  <asp:TableRow runat="server">
+                       <asp:TableCell runat="server"> <asp:Label ID="Label7" runat="server" > </asp:Label> </asp:TableCell>
+                  </asp:TableRow>
+                  <asp:TableRow runat="server">
+                  </asp:TableRow>
+              </asp:Table>
+
+            
+
+
+             <br />
+              <br />
+              <asp:Label ID="Label6" runat="server" Text="MERVE AYDIN ASP.NET EĞİTİM" > </asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ UstSoySorgulama.aspx.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UstSoySorgula
+{
+    public partial class UstSoySorgulama : System.Web.UI.Page
+    {
+        public int MaxLength { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //private void TxtTcKimlikNo_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        //}
+
+        protected void Sorgula_Click(object sender, EventArgs e)
+        {
+            if (txtTcKimlikNo.Text == "")
+            {
+                Label5.Text = ("Tc Kimlik Numarası Giriniz !! ");
+                Label5.ForeColor = System.Drawing.Color.Red;
+                txtTcKimlikNo.Focus();
+            }
+            
+            else if (txtTcKimlikNo.Text.Length < 11)
+            {
+                Label5.Text = ("HATALI  TC KİMLİK NUMARASI !! ");
+                Label5.ForeColor = System.Drawing.Color.Red;
+                txtTcKimlikNo.Focus();
+
+            }
+           
+            else if (txtAd.Text == "")
+            {
+                Label5.Text = ("Adınızı  Giriniz !! ");
+                Label5.ForeColor = System.Drawing.Color.Red;
+                txtAd.Focus();
+
+
+
+            }
+
+            else if (txtSoyad.Text == "")
+            {
+                Label5.Text = ("Soyadınızı  Giriniz !! ");
+                Label5.ForeColor = System.Drawing.Color.Red;
+                txtSoyad.Focus();
+
+
+            }
+            else if (txtDogumYili.Text == "")
+            {
+                Label5.Text = ("Doğum yılınızı  Giriniz !! ");
+                Label5.ForeColor = System.Drawing.Color.Red;
+                txtDogumYili.Focus();
+
+
+
+            }
+            else if (txtDogumYili.Text.Length < 4 )
+            {
+                Label5.Text = ("HATALI DOĞUM YILI !! ");
+                Label5.ForeColor = System.Drawing.Color.Red;
+                txtDogumYili.Focus();
+
+
+
+            }
+
+
+
+            else
+            {
+                // ilk 4 haneyi alıp 3 ile toplama
+                String tcKimlikNO = txtTcKimlikNo.Text; ;
+                char[] ayir = new char[11];
+                ayir = tcKimlikNO.ToCharArray();
+                
+                 string stepOne = ayir[0].ToString() + ayir[1].ToString() + ayir[3].ToString() + ayir[3].ToString() + ayir[4].ToString();               
+                int x = Int32.Parse(stepOne);
+                int StepOne = x + 3;
+               
+
+
+
+                // sonraki 4 haneden 1 çıkarma
+                string StepTwoEkle = ayir[5].ToString() + ayir[6].ToString() + ayir[7].ToString() + ayir[8].ToString();
+                int y = Int32.Parse(StepTwoEkle);
+                int StepTwo = y - 1; 
+                
+                // son rakamdan 4 çıkartma
+                string tckimlikNoYeni;
+                string tcsonRakam = ayir[10].ToString();
+                int sonrakam = Int32.Parse(tcsonRakam);
+                int StepThree = 0;
+
+                if (sonrakam < 4)
+
+                {
+                    string sonRakaminBasina1Ekle = "1" + tcsonRakam;
+                    /// Response.Write(" son rakama 1 +  " + sonRakaminBasina1Ekle + "</br>");
+                    StepThree = Int32.Parse(sonRakaminBasina1Ekle) - 4;
+                    ///  Response.Write(" son rakama 2 +  " + z);
+
+                    tckimlikNoYeni = Convert.ToString(StepOne) + Convert.ToString(StepTwo) + Convert.ToString(StepThree);
+                    /// Response.Write("tc  " + tckimlikNoYeni + "</br>");
+
+                }
+                else
+
+                {
+                    string sonRakaminBasina1Ekle = tcsonRakam;
+                    StepThree = Int32.Parse(sonRakaminBasina1Ekle) - 4;
+                    //  Response.Write(" son rakama 2 +  " + z + "</br>");
+                    tckimlikNoYeni = Convert.ToString(StepOne) + Convert.ToString(StepTwo) + Convert.ToString(StepThree);
+                    //  Response.Write("tc  " + tckimlikNoYeni + "</br>");
+                }
+
+                // üretilen tc i diziye atama
+                char[] birlestir = new char[11];
+                birlestir = tckimlikNoYeni.ToCharArray();
+                string sDonustur;
+                int ata = 0;
+                int topla = 0;
+
+                for (int i = 0; i < birlestir.Length; i++)
+                {
+
+                    sDonustur = Convert.ToString(birlestir[i]);
+                    ata = Int32.Parse(sDonustur);
+
+                    topla = topla + ata;
+                }
+
+                // Response.Write(topla + "</br>");
+                string sConvert = Convert.ToString(topla);
+                char[] cConvert = new char[1];
+                cConvert = sConvert.ToCharArray();
+                string xxx = Convert.ToString(sConvert[1]);
+                int deger = Int32.Parse(xxx);
+                /// Response.Write(deger + "</br>");
+                int StepFour = 0;
+
+                while (deger != 8)
+                {
+                    deger++;
+                    StepFour++;
+
+                }
+
+                ///Response.Write(deger + "</br>");
+                /// Response.Write(sayac + "</br>");
+
+                String Tc = Convert.ToString(StepOne) + Convert.ToString(StepTwo) + Convert.ToString(StepFour) + Convert.ToString(StepThree);
+                // Response.Write(" yeni tc " + Tc + "</br>");
+
+
+
+
+
+                // Kimliği doğrulama 
+                long tckimlik = long.Parse(Tc);
+                int dogumyili = int.Parse(txtDogumYili.Text);
+
+                bool? durum;
+
+                //Response.Write(tckimlik + "</br>");
+
+
+                try
+                {
+                    using (KimlikDogrulamasi.KPSPublicSoapClient servis = new KimlikDogrulamasi.KPSPublicSoapClient())
+                    {
+                        durum = servis.TCKimlikNoDogrula(tckimlik, txtAd.Text.ToUpper(), txtSoyad.Text.ToUpper(), dogumyili);
+                    }
+                }
+                catch
+                {
+                    Label7.Text = "";
+                    durum = null;
+                    Label5.Text = ("Hata var.");
+                    Label5.ForeColor = System.Drawing.Color.Red;
+                    txtTcKimlikNo.Focus();
+                }
+                if (durum == true)
+                {
+                    Label5.Text = "";
+                    Label7.Text = (txtAd.Text + " " + txtSoyad.Text + "T.C. Kimlik Doğrudur");
+                    Label7.ForeColor = System.Drawing.Color.Green;
+                    txtTcKimlikNo.Focus();
+                }
+                else
+                {
+
+                    Label5.Text = ("Böyle bir T.C. Kimlik No Bulunmamaktadır");
+                    Label5.ForeColor = System.Drawing.Color.Red;
+                    Label7.Text = "";
+                    txtTcKimlikNo.Focus();
+                }
+            }
+        }
+
+       
+    }
+}
